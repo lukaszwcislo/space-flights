e9bf74a7b26dce38e105a3ead6f3526a5c98a886
lukaszwcislo <lukasz.wcislo87@gmail.com>
2023-01-07 22:01:02 +0100

    added dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rockets';
+
+  ngOnInit() {
+    if(localStorage.hasOwnProperty('darkMode')) {
+      if(JSON.parse(localStorage["darkMode"])) {
+        document.body.setAttribute('data-dark-mode', '')
+      }
+    }
+  }
 }
--- src/app/core/dashboard/dashboard.component.ts
+++ src/app/core/dashboard/dashboard.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  public isSetDarkMode!: boolean;
+
+  public changeAppTheme() {
+    const body = document.body;
+
+    if(localStorage.hasOwnProperty('darkMode')) {
+      if(JSON.parse(localStorage["darkMode"]) === true) {
+        body.removeAttribute('data-dark-mode');
+        localStorage.setItem('darkMode', 'false')
+        this.isSetDarkMode = false;
+      } else {
+        body.setAttribute('data-dark-mode', '')
+        localStorage.setItem('darkMode', 'true');
+        this.isSetDarkMode = true;
+      }
+    } else {
+      body.setAttribute('data-dark-mode', '');
+      localStorage.setItem('darkMode', 'true');
+      this.isSetDarkMode = true;
+    }
+  }
+
+  ngOnInit(): void {
+    if(localStorage.hasOwnProperty('darkMode')) {
+      if(JSON.parse(localStorage["darkMode"]) === true) {
+        this.isSetDarkMode = true;
+      } else {
+        this.isSetDarkMode = false;
+      }
+    } else {
+      this.isSetDarkMode = false;
+    }
+  }
 }
